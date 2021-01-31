e74b8d1c16c69ba408ec0f59284521b4997dc13f
The_Fireplace <the.f1repl4ce@gmail.com>
2021-01-30 23:23:48 -0600

    Add javaDoc to demonstrate exception

--- src/main/java/net/fabricmc/example/ExampleMod.java
+++ src/main/java/net/fabricmc/example/ExampleMod.java
@@ -3,6 +3,9 @@ package net.fabricmc.example;
 import net.fabricmc.api.ModInitializer;
 
 public class ExampleMod implements ModInitializer {
+	/**
+	 * Want to see an exception when building? Leave this link here! {@link net.fabricmc.example}
+	 */
 	@Override
 	public void onInitialize() {
 		// This code runs as soon as Minecraft is in a mod-load-ready state.
